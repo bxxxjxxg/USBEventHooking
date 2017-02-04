4d706ea603c1a624c29fc5f647b6c994d5241fa4
bxxxjxxg <r00922040@ntu.edu.tw>
2017-02-04 22:27:52 +0800

    Implement usb event hooking for arrival and removal events

---
+++ USBEventHooking/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace USBEventHooking
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            outputTextBox.AppendText("Form::Onload()\r\n");
+            UsbEventHooker.RegisterNotification(this.Handle, UsbEventHooker.RecipientType.WINDOW_PROGRAM);
+            outputTextBox.AppendText("UsbEventHooker.RegisterNotification() ... done! \r\n");
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            if (m.Msg == UsbEventHooker.WmDevicechange)
+            {
+                switch ((int)m.WParam)
+                {
+                    case UsbEventHooker.DbtDeviceremovecomplete:
+                        outputTextBox.AppendText("UsbEventHooker.DbtDeviceremovecomplete\r\n");
+                        break;
+                    case UsbEventHooker.DbtDevicearrival:
+                        outputTextBox.AppendText("UsbEventHooker.DbtDevicearrival\r\n");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ USBEventHooking/UsbEventHooker.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace USBEventHooking
+{
+    static class UsbEventHooker
+    {
+        public enum RecipientType
+        {
+            WINDOW_PROGRAM,
+            SERVICE_PROGRAM
+        }
+
+        public const int DbtDevicearrival = 0x8000;        // system detected a new device        
+        public const int DbtDeviceremovecomplete = 0x8004; // device is gone      
+        public const int WmDevicechange = 0x0219;          // device change event
+        public const int DEVICE_NOTIFY_WINDOW_HANDLE = 0x00000000;
+        public const int DEVICE_NOTIFY_SERVICE_HANDLE = 0x00000001;
+
+        private const int DbtDevtypDeviceinterface = 5;
+        private static readonly Guid GuidDevinterfaceUSBDevice = new Guid("A5DCBF10-6530-11D2-901F-00C04FB951ED"); // USB devices
+        private static IntPtr notificationHandle;
+
+        public static void RegisterNotification(IntPtr windowHandle, RecipientType type)
+        {
+            DevBroadcastDeviceinterface dbi = new DevBroadcastDeviceinterface
+            {
+                DeviceType = DbtDevtypDeviceinterface,
+                Reserved = 0,
+                ClassGuid = GuidDevinterfaceUSBDevice,
+                Name = 0
+            };
+
+            dbi.Size = Marshal.SizeOf(dbi);
+            IntPtr buffer = Marshal.AllocHGlobal(dbi.Size);
+            Marshal.StructureToPtr(dbi, buffer, true);
+
+            if (type == RecipientType.WINDOW_PROGRAM)
+            {
+                notificationHandle = RegisterDeviceNotification(windowHandle, buffer, DEVICE_NOTIFY_WINDOW_HANDLE);
+            }
+            else
+            {
+                notificationHandle = RegisterDeviceNotification(windowHandle, buffer, DEVICE_NOTIFY_SERVICE_HANDLE);
+            }
+            
+        }
+
+        public static void UnregisterUsbDeviceNotification()
+        {
+            UnregisterDeviceNotification(notificationHandle);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr RegisterDeviceNotification(IntPtr recipient, IntPtr notificationFilter, int flags);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterDeviceNotification(IntPtr handle);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct DevBroadcastDeviceinterface
+        {
+            internal int Size;
+            internal int DeviceType;
+            internal int Reserved;
+            internal Guid ClassGuid;
+            internal short Name;
+        }
+
+    }
+}
