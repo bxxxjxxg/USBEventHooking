a724e7e74210d3c36bea55ceae31a39882cd172c
bxxxjxxg <r00922040@ntu.edu.tw>
2017-02-05 21:56:30 +0800

    implement using WqlEventQuery to watch use insertion/removal events

--- USBEventHooking/Form1.cs
+++ USBEventHooking/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Management;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,8 +21,24 @@ namespace USBEventHooking
         private void Form1_Load(object sender, EventArgs e)
         {
             outputTextBox.AppendText("Form::Onload()\r\n");
-            UsbEventHooker.RegisterNotification(this.Handle, UsbEventHooker.RecipientType.WINDOW_PROGRAM);
-            outputTextBox.AppendText("UsbEventHooker.RegisterNotification() ... done! \r\n");
+
+            // Method 1
+            // UsbEventHooker.RegisterNotification(this.Handle, UsbEventHooker.RecipientType.WINDOW_PROGRAM);
+            // outputTextBox.AppendText("UsbEventHooker.RegisterNotification() ... done! \r\n");
+
+            // Method 2
+            WqlEventQuery insertQuery = new WqlEventQuery("SELECT * FROM __InstanceCreationEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_USBHub'");
+
+            ManagementEventWatcher insertWatcher = new ManagementEventWatcher(insertQuery);
+            insertWatcher.EventArrived += new EventArrivedEventHandler(DeviceInsertedEvent);
+            insertWatcher.Start();
+            outputTextBox.AppendText("WqlEventQuery for insertion events... done! \r\n");
+
+            WqlEventQuery removeQuery = new WqlEventQuery("SELECT * FROM __InstanceDeletionEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_USBHub'");
+            ManagementEventWatcher removeWatcher = new ManagementEventWatcher(removeQuery);
+            removeWatcher.EventArrived += new EventArrivedEventHandler(DeviceRemovedEvent);
+            removeWatcher.Start();
+            outputTextBox.AppendText("WqlEventQuery for removal events ... done! \r\n");
         }
 
         protected override void WndProc(ref Message m)
@@ -38,6 +55,24 @@ namespace USBEventHooking
                         outputTextBox.AppendText("UsbEventHooker.DbtDevicearrival\r\n");
                         break;
                 }
+            }
+        }
+
+        private void DeviceInsertedEvent(object sender, EventArrivedEventArgs e)
+        {
+            ManagementBaseObject instance = (ManagementBaseObject)e.NewEvent["TargetInstance"];
+            foreach (var property in instance.Properties)
+            {
+                Console.WriteLine(property.Name + " = " + property.Value);
+            }
+        }
+
+        private void DeviceRemovedEvent(object sender, EventArrivedEventArgs e)
+        {
+            ManagementBaseObject instance = (ManagementBaseObject)e.NewEvent["TargetInstance"];
+            foreach (var property in instance.Properties)
+            {
+                Console.WriteLine(property.Name + " = " + property.Value);
             }
         }
     }
